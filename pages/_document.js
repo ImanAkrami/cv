3f66d91c4f4900242e3e63c2b30d0a9b46931760
Iman Akrami <iman.akrami@gmail.com>
2022-12-16 23:14:47 +0330

    fx

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: 'Next.js Blog Starter Kit',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
-    image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
+    title: 'Iman Akrami CV',
+    description: 'Web developer, educational technologist, and a dreamer.',
+    image: '/image/meta.png'
   }
 
   return (
